b522f0bc5b025ea3f5e57f0a76e16bee4debbede
DmitryKashkin <7842066@gmail.com>
2022-05-20 19:56:28 +0300

    work

--- xml-to-json.py
+++ xml-to-json.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as eT
-sample_xml = ['BARCODE', 'PRICE', 'NAME', 'PLU']
+# Создаем пары соответствия названий xml-json
 dict0 = {'BARCODE': '"barcode":', 'PRICE': '"price":',
          'NAME': ',"name":"', 'PLU': '","code":'}
 prefix = '{"baseVer":"0.4","base":['
@@ -13,9 +13,10 @@ f.truncate(0)
 f.write(prefix)
 for tag in root_node.findall('WARES/WARE'):
     item += item0[0]
-    for i in sample_xml:
+    for i in dict0.keys():
         if i == 'PRICE':
             item += dict0[i] + str(int(tag.get(i)) * 100)
+            # цена в xml в рублях, в json в копейках
             continue
         item += dict0[i] + tag.get(i)
         if i == 'BARCODE':
@@ -23,10 +24,12 @@ for tag in root_node.findall('WARES/WARE'):
     item += item0[2]
     f.write(item)
     item = ''
+# удаляем лишнюю запятую в конце
 f.seek(f.tell()-1)
 f.truncate()
 f.write(suffix)
 f.close()
+# перекодируем в utf8
 f = open('prazdnik.json', 'rb+')
 item = f.read()
 item = item.decode('cp1251').encode('utf8')
