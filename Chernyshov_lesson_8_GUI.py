ea2c8d218a1449d7e1287405d05a6405ded42a05
DmitryKashkin <7842066@gmail.com>
2022-08-08 21:39:07 +0300

    less 8. v1 Сделано 8.2.4 (опросник)

--- Chernyshov_lesson_8_GUI.py
+++ Chernyshov_lesson_8_GUI.py
@@ -1,12 +1,12 @@
 import random
 import sys
-from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit
+from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QCheckBox
 from PySide2.QtCore import Qt
 
 
 class LoginWindow(QWidget):
     def __init__(self):
-        super().__init__()
+        super(LoginWindow, self).__init__()
         self.line_edit = self.button = self.lable_one = self.lable_two = None
         self.lables()
         self.buttons()
@@ -15,7 +15,7 @@ class LoginWindow(QWidget):
 
     def line_edits(self):
         self.line_edit = QLineEdit(self)
-        self.line_edit.setAlignment(Qt.AlignLeft)
+        self.line_edit.setAlignment(Qt.AlignCenter)
         self.line_edit.move(130, 50)
         self.line_edit.resize(200, 20)
 
@@ -26,6 +26,7 @@ class LoginWindow(QWidget):
 
     def lables(self):
         self.lable_one = QLabel('Enter Login', self)
+        self.lable_one.setAlignment(Qt.AlignCenter)
         self.lable_one.move(130, 20)
         self.lable_one.resize(200, 20)
         self.lable_two = QLabel('Login', self)
@@ -43,7 +44,70 @@ class LoginWindow(QWidget):
             self.lable_one.setText(str(random.random()))
 
 
+class CheckBoxWindow(QWidget):
+    def __init__(self):
+        super(CheckBoxWindow, self).__init__()
+        self.lables()
+        self.check_boxes()
+        self.window_set()
+
+    def window_set(self):
+        self.setGeometry(1000, 500, 400, 300)
+        self.setWindowTitle('the survey')
+        self.show()
+
+    def lables(self):
+        self.lable_one = QLabel('What time do you wake up?', self)
+        self.lable_one.move(20, 20)
+        self.lable_one.resize(200, 20)
+        self.lable_two = QLabel('choose something', self)
+        self.lable_two.move(20, 250)
+        self.lable_two.setMinimumSize(200, 20)
+
+    def check_boxes(self):
+        morning_6 = QCheckBox('6 utra', self)
+        morning_6.move(20, 80)
+        morning_6.stateChanged.connect(self.print_choise)
+        morning_7 = QCheckBox('7 utra', self)
+        morning_7.move(20, 100)
+        morning_7.stateChanged.connect(self.print_choise)
+        never_sleep = QCheckBox('I never sleep', self)
+        never_sleep.move(20, 120)
+        never_sleep.stateChanged.connect(self.print_choise)
+
+    def print_choise(self, state):
+        sender = self.sender()
+        if state == Qt.Checked:
+            choose = f'Selected: {sender.text()}.'
+        else:
+            choose = f'Unselected: {sender.text()}.'
+        self.lable_two.setText(choose)
+
+
+style = """
+    QWidget {
+        background-color: "green";
+        color: "white";
+    }
+    QPushButton {
+        font-size: 16px;
+        background-color: "darkgreen"
+    }
+    QLineEdit {
+        background-color: "white";
+        color: "black";
+    }
+    QLabel {
+        font-size: 12pt;
+    }
+    QCheckBox {
+        font-size: 12pt;
+    }
+"""
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = LoginWindow()
+    app.setStyleSheet(style)
+
+    window = CheckBoxWindow()
     sys.exit(app.exec_())
