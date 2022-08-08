9961bd8de8bf198e5e8be71f542a7576fcda0504
DmitryKashkin <7842066@gmail.com>
2022-08-08 19:22:30 +0300

    less 8. Login Window. v0.2

--- Chernyshov_lesson_8_GUI.py
+++ Chernyshov_lesson_8_GUI.py
@@ -1,32 +1,37 @@
 import sys
-from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit
+from PySide2.QtCore import Qt
 
 
-class MainWindow(QWidget):
-    def __init__(self, *args):
+class LoginWindow(QWidget):
+    def __init__(self):
         super().__init__()
-        self.setGeometry(*args)
-        self.setWindowTitle('My window')
-        self.lable = QLabel(self)
-        self.lable.move(60, 30)
-        self.lable.setText('Press')
-        self.button = QPushButton('Ok', self)
-        self.button.move(80, 70)
-        self.button.clicked.connect(self.button_clicked)
+        QLabel('Enter Login', self).move(130, 20)
+        QLabel('Login', self).move(90, 55)
+        self.button = QPushButton('Clear', self)
+        self.button.move(160, 110)
+        self.line_edit = QLineEdit(self)
+        self.line_edit.setAlignment(Qt.AlignLeft)
+        self.line_edit.move(130, 50)
+        self.line_edit.resize(200, 20)
+        self.button.clicked.connect(self.clear_entries)
+        self.window_set()
+
+
+
+    def window_set(self):
+        self.setGeometry(1000, 500, 400, 300)
+        self.setWindowTitle('Login window')
         self.show()
 
-        # self.initialize_ui(*args)
 
-    # def initialize_ui(self, *args):
-
-    def button_clicked(self):
-        self.lable.setText('yes')
-        self.lable.move(70, 40)
-        self.button.setText('Ya!')
-        self.button.move(90, 80)
+    def clear_entries(self):
+        sender = self.sender()
+        if sender.text() == 'Clear':
+            self.line_edit.clear()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MainWindow(100, 100, 400, 300)
+    window = LoginWindow()
     sys.exit(app.exec_())
