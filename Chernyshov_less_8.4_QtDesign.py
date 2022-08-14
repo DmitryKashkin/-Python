00ae0d9e26b1e7e1b052957af80ce240598c18f0
DmitryKashkin <7842066@gmail.com>
2022-08-14 20:55:53 +0300

    less 8. v5 8.4. End. Work

--- Chernyshov_less_8.4_QtDesign.py
+++ Chernyshov_less_8.4_QtDesign.py
@@ -5,15 +5,27 @@ from QuestionareWidget import Ui_QuestionareWidget
 
 
 class MainWindow(QWidget):
-    ratings = {'0': 'Ужасно', '1': 'Так себе', '2': 'Норм', '3': 'Отл'}
+    ratings = {'1': 'Ужасно', '2': 'Так себе', '3': 'Норм', '4': 'Отл'}
+
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.ui=Ui_QuestionareWidget
-        # self.ui.setupUI(self)
+        self.ui = Ui_QuestionareWidget()
+        self.ui.setupUi(self)
+        scale_bg = QButtonGroup(self)
+        scale_bg.addButton(self.ui.checkBox)
+        scale_bg.addButton(self.ui.checkBox_2)
+        scale_bg.addButton(self.ui.checkBox_3)
+        scale_bg.addButton(self.ui.checkBox_4)
+        scale_bg.buttonClicked.connect(self.checkboxClicked)
+        close_button=self.ui.pushButton
+        close_button.clicked.connect(self.close)
+
+    def checkboxClicked(self, cb: QCheckBox):
+        self.ui.label_7.setText(f'Выбрано: {self.ratings[cb.text()]} {cb.text()}')
 
 
 if __name__ == '__main__':
-    app=QApplication(sys.argv)
-    window=MainWindow()
+    app = QApplication(sys.argv)
+    window = MainWindow()
     window.show()
     sys.exit(app.exec_())
