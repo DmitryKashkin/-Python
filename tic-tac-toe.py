1935cd5dba3748fd918b58a4918ed9b10c19dbd2
admin <7842066@gmail.com>
2022-08-29 19:27:30 +0300

    tic-tac-toe v0.9 - исправить баг: комп ходит два раза

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,5 +1,6 @@
 import sys
 import time
+from random import randint
 
 from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QLabel, \
     QLineEdit, QCheckBox, QButtonGroup, QHBoxLayout, QVBoxLayout, QTextEdit, QGridLayout, \
@@ -13,7 +14,7 @@ from TicTacToe import Ui_MainWindow
 
 
 class WarningDialog(QDialog):
-    def __init__(self):
+    def __init__(self, label):
         super().__init__()
         self.setWindowTitle("WARNING!")
         QBtn = QDialogButtonBox.StandardButton.Ok
@@ -21,7 +22,8 @@ class WarningDialog(QDialog):
         self.buttonBox.accepted.connect(self.accept)
         self.buttonBox.rejected.connect(self.reject)
         self.layout = QVBoxLayout()
-        message = QLabel("СЮДА НЕ ХОДИ!")
+        message = QLabel(label)
+        # message = QLabel("СЮДА НЕ ХОДИ!")
         self.layout.addWidget(message)
         self.layout.addWidget(self.buttonBox)
         self.setLayout(self.layout)
@@ -29,13 +31,22 @@ class WarningDialog(QDialog):
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
+        self.is_new_game = True
         self.i_am = ''
+        self.i_am_comp = ''
         self.dict_i_am = {'X': 1, '0': -1}
         self.game_board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.button_list = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
         self.i = 0
         self.j = 0
         super(MainWindow, self).__init__()
         self.setupUi(self)
+        for btn in self.buttonGroup.buttons():
+            j = int(btn.objectName()[-2])
+            i = int(btn.objectName()[-1])
+            self.button_list[j][i] = btn
+
         self.buttonGroup.buttonClicked.connect(self.click_btn)
         self.actionNew_Game.triggered.connect(self.new_game)
         self.new_game()
@@ -44,18 +55,34 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if btn.text() != '':
             self.warning_mesage()
         else:
+            self.is_new_game = False
             btn.setText(self.i_am)
             self.j = int(btn.objectName()[-2])
             self.i = int(btn.objectName()[-1])
             self.game_board[self.j][self.i] = self.dict_i_am[self.i_am]
             self.check_for_victory()
+            self.next_move()
             print(self.game_board)
 
+    def next_move(self):
+        if self.is_new_game:
+            return
+        while True:
+            self.i = randint(0, 2)
+            self.j = randint(0, 2)
+            if self.game_board[self.j][self.i] == 0:
+                self.game_board[self.j][self.i] = self.dict_i_am[self.i_am_comp]
+                btn = self.button_list[self.j][self.i]
+                btn.setText(self.i_am_comp)
+                break
+        self.check_for_victory()
 
     def victory(self):
         print('Victory')
+        message = 'Победил ' + self.button_list[self.j][self.i].text()
+        dlg = WarningDialog(message)
+        dlg.exec()
         self.new_game()
-
 
     def check_for_victory(self):
         sum_1 = self.game_board[self.j][0] + self.game_board[self.j][1] + self.game_board[self.j][2]
@@ -67,7 +94,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             return
 
     def warning_mesage(self):
-        dlg = WarningDialog()
+        dlg = WarningDialog("СЮДА НЕ ХОДИ!")
         dlg.exec()
 
     def clear(self):
@@ -76,11 +103,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.game_board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
     def new_game(self):
+        self.clear()
+        self.is_new_game = True
+        self.i_am_comp = '0'
         options = ("X", "0")
         pressed = False
         while not pressed:
             self.i_am, pressed = QInputDialog.getItem(self, "Вы за кого?", "Вы за кого?", options, 0, False)
-        self.clear()
+        if self.i_am == '0':
+            self.i_am_comp = 'X'
+            self.is_new_game = False
+            self.next_move()
 
 
 if __name__ == '__main__':
